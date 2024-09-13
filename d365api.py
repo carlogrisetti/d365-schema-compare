246ec24f8675d572982905a97300d63618db7639
Carlo Grisetti <carlo.grisetti@gmail.com>
2024-09-13 17:55:54 +0200

    Initial commit

---
+++ d365api.py
@@ -0,0 +1,200 @@
+from urllib.parse import urlencode
+import re
+
+import requests
+
+
+class Client:
+    api_path = "api/data/v9.2"
+
+    def __init__(self, domain, client_id=None, client_secret=None, access_token=None):
+        self.domain = domain.strip("/")
+        self.scopes = [f"{domain}/user_impersonation"]
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.access_token = access_token
+
+        self.headers = {
+            "Accept": "application/json, */*",
+            "OData-MaxVersion": "4.0",
+            "OData-Version": "4.0",
+        }
+
+        if access_token is not None:
+            self.set_access_token(access_token)
+
+    def set_access_token(self, token):
+        """
+        Sets the Token for its use in this library.
+        :param token: A string with the Token.
+        :return:
+        """
+        assert token is not None, "The token cannot be None."
+        self.access_token = token
+        self.headers["Authorization"] = "Bearer " + self.access_token
+
+    def make_request(
+        self,
+        method,
+        endpoint,
+        expand=None,
+        filter=None,
+        orderby=None,
+        select=None,
+        skip=None,
+        top=None,
+        data=None,
+        json=None,
+        **kwargs,
+    ):
+        """
+        this method do the request petition, receive the different methods (post, delete, patch, get) that the api allow, see the documentation to check how to use the filters
+        https://msdn.microsoft.com/en-us/library/gg309461(v=crm.7).aspx
+        :param method:
+        :param endpoint:
+        :param expand:
+        :param filter:
+        :param orderby:
+        :param select:
+        :param skip:
+        :param top:
+        :param data:
+        :param json:
+        :param kwargs:
+        :return:
+        """
+        extra = {}
+        if expand is not None and isinstance(expand, str):
+            extra["$expand"] = str(expand)
+        if filter is not None and isinstance(filter, str):
+            extra["$filter"] = filter
+        if orderby is not None and isinstance(orderby, str):
+            extra["$orderby"] = orderby
+        if select is not None and isinstance(select, str):
+            extra["$select"] = select
+        if skip is not None and isinstance(skip, str):
+            extra["$skip"] = skip
+        if top is not None and isinstance(top, str):
+            extra["$top"] = str(top)
+
+        assert self.domain is not None, "'domain' is required"
+        assert self.access_token is not None, "You must provide a 'token' to make requests"
+        url = f"{self.domain}/{self.api_path}/{endpoint}?" + urlencode(extra)
+        if method == "get":
+            response = requests.request(method, url, headers=self.headers, params=kwargs)
+        else:
+            response = requests.request(method, url, headers=self.headers, data=data, json=json)
+
+        return self.parse_response(response)
+
+    def _get(self, endpoint, data=None, **kwargs):
+        return self.make_request("get", endpoint, data=data, **kwargs)
+
+    def _post(self, endpoint, data=None, json=None, **kwargs):
+        return self.make_request("post", endpoint, data=data, json=json, **kwargs)
+
+    def _delete(self, endpoint, **kwargs):
+        return self.make_request("delete", endpoint, **kwargs)
+
+    def _patch(self, endpoint, data=None, json=None, **kwargs):
+        return self.make_request("patch", endpoint, data=data, json=json, **kwargs)
+
+    def parse_response(self, response):
+        """
+        This method get the response request and returns json data or raise exceptions
+        :param response:
+        :return:
+        """
+        if response.status_code == 204 or response.status_code == 201:
+            if 'OData-EntityId' in response.headers:
+                entity_id = response.headers['OData-EntityId']
+                if entity_id[-38:-37] == '(' and entity_id[-1:] == ')':  # Check container
+                    guid = entity_id[-37:-1]
+                    guid_pattern = re.compile(r'^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}$', re.IGNORECASE)
+                    if guid_pattern.match(guid):
+                        return guid
+                    else:
+                        return True  # Not all calls return a guid
+            else:
+                return True
+        elif response.status_code == 400:
+            raise Exception(
+                "The URL {0} retrieved an {1} error. Please check your request body and try again.\nRaw message: {2}".format(
+                    response.url, response.status_code, response.text
+                )
+            )
+        elif response.status_code == 401:
+            raise Exception(
+                "The URL {0} retrieved and {1} error. Please check your credentials, make sure you have permission to perform this action and try again.".format(
+                    response.url, response.status_code
+                )
+            )
+        elif response.status_code == 403:
+            raise Exception(
+                "The URL {0} retrieved and {1} error. Please check your credentials, make sure you have permission to perform this action and try again.".format(
+                    response.url, response.status_code
+                )
+            )
+        elif response.status_code == 404:
+            raise Exception(
+                "The URL {0} retrieved an {1} error. Please check the URL and try again.\nRaw message: {2}".format(
+                    response.url, response.status_code, response.text
+                )
+            )
+        elif response.status_code == 412:
+            raise Exception(
+                "The URL {0} retrieved an {1} error. Please check the URL and try again.\nRaw message: {2}".format(
+                    response.url, response.status_code, response.text
+                )
+            )
+        elif response.status_code == 413:
+            raise Exception(
+                "The URL {0} retrieved an {1} error. Please check the URL and try again.\nRaw message: {2}".format(
+                    response.url, response.status_code, response.text
+                )
+            )
+        elif response.status_code == 500:
+            raise Exception(
+                "The URL {0} retrieved an {1} error. Please check the URL and try again.\nRaw message: {2}".format(
+                    response.url, response.status_code, response.text
+                )
+            )
+        elif response.status_code == 501:
+            raise Exception(
+                "The URL {0} retrieved an {1} error. Please check the URL and try again.\nRaw message: {2}".format(
+                    response.url, response.status_code, response.text
+                )
+            )
+        elif response.status_code == 503:
+            raise Exception(
+                "The URL {0} retrieved an {1} error. Please check the URL and try again.\nRaw message: {2}".format(
+                    response.url, response.status_code, response.text
+                )
+            )
+        return response.json()
+
+    def get_data(self, type=None, **kwargs):
+        if type is not None:
+            return self._get(type, **kwargs)
+        raise Exception("A type is necessary. Example: contacts, leads, accounts, etc... check the library")
+
+    def create_data(self, type=None, **kwargs):
+        if type is not None and kwargs is not None:
+            params = {}
+            params.update(kwargs)
+            return self._post(type, json=params)
+        raise Exception("A type is necessary. Example: contacts, leads, accounts, etc... check the library")
+
+    def update_data(self, type=None, id=None, **kwargs):
+        if type is not None and id is not None:
+            url = "{0}({1})".format(type, id)
+            params = {}
+            if kwargs is not None:
+                params.update(kwargs)
+            return self._patch(url, json=params)
+        raise Exception("A type is necessary. Example: contacts, leads, accounts, etc... check the library")
+
+    def delete_data(self, type=None, id=None):
+        if type is not None and id is not None:
+            return self._delete("{0}({1})".format(type, id))
+        raise Exception("A type is necessary. Example: contacts, leads, accounts, etc... check the library")
---
+++ main.py
@@ -0,0 +1,146 @@
+import yaml
+import adal
+import time
+import json
+from d365api import Client
+
+CONFIG_FILEPATH = 'config.yaml'
+
+
+class ClientManager:
+    access_token = None
+    access_token_refresh_time = None
+    tenant_id = None
+    client_url = None
+    client_id = None
+    client_secret = None
+    client_object = None
+
+    def __init__(self, tenant_id: str, client_url: str, client_id: str, client_secret: str):
+        self.tenant_id = tenant_id
+        self.client_url = client_url
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def get_access_token(self):
+        if self.access_token_refresh_time is None or (time.time() - self.access_token_refresh_time) > 60:  # If N seconds have passed, get a new token
+            authority_url = 'https://login.microsoftonline.com/' + self.tenant_id
+            context = adal.AuthenticationContext(authority_url)
+            token = context.acquire_token_with_client_credentials(
+                resource=self.client_url,
+                client_id=self.client_id,
+                client_secret=self.client_secret
+            )
+            self.access_token = token["accessToken"]
+            self.access_token_refresh_time = time.time()
+        # In any case return the token object
+        return self.access_token
+
+    def get_client_object(self):
+        self.get_access_token()
+        if self.client_object is None:  # First time being called
+            self.client_object = Client(domain=self.client_url, access_token=self.access_token)
+        else:
+            self.client_object.set_access_token(token=self.access_token)
+        return self.client_object  # In any case return the client object
+
+
+# Print iterations progress
+def print_progress_bar(start_time: float, iteration, total, prefix='', decimals=1, length=100, fill='█', end="\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        start_time  - Required  : starting date\time to compute remaining time to completion (Float)
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        end         - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percentage = (100 * (iteration / float(total)))
+    percentage_str = ("{0:." + str(decimals) + "f}").format(percentage)
+
+    elapsed_time = time.time() - start_time
+    total_time = ((elapsed_time * 100) / percentage) if percentage > 0 else 0  # Avoid division by zero
+    remaining_time = (total_time - elapsed_time) if (total_time - elapsed_time) >= 0 else 0
+    elapsed_str = time.strftime('%M:%S', time.gmtime(elapsed_time))
+    remaining_str = time.strftime('%M:%S', time.gmtime(remaining_time))
+    elapsed_and_remaining = f"[{elapsed_str}-{remaining_str}]"
+
+    speed = int((iteration / elapsed_time) * 60) if elapsed_time > 0 else 0  # Avoid division by zero
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    iter_vs_tot = f'{iteration}/{total}'
+    print(f'\r{prefix} |{bar}| {iter_vs_tot.ljust(9)} ({percentage_str}%) {speed}/min {elapsed_and_remaining}', end=end)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
+def get_entity_definitions(client_manager: ClientManager):
+    time_start = time.time()
+
+    client = client_manager.get_client_object()
+    result = client.make_request(
+        method='get',
+        endpoint='EntityDefinitions',
+        select='LogicalName',
+        expand='Attributes',
+    )
+
+    time_end = time.time()
+    time_taken = time_end - time_start
+    return result, time_taken
+
+
+def main():
+
+    # region Config
+    with open(CONFIG_FILEPATH) as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+
+        if 'environment' in config:
+            tenant_id = config['environment']['tenant_id']
+            client_url = config['environment']['client_url']
+            client_id = config['environment']['client_id']
+            client_secret = config['environment']['client_secret']
+        else:
+            raise SyntaxError("Configuration incorrect")
+    # endregion
+
+    # Let's start by stating some obvious facts
+    print(f"Configuration file: {CONFIG_FILEPATH}")
+
+    # Logging in to the system
+    print("==> Authenticating on Dynamics 365...")
+    client_manager = ClientManager(
+        tenant_id=tenant_id,
+        client_url=client_url,
+        client_id=client_id,
+        client_secret=client_secret
+    )
+
+    # Getting entity structure
+    print("==> Retrieving Entity Definitions...")
+    result, time_taken = get_entity_definitions(client_manager=client_manager)
+    print(f"==> It took {time_taken:.1f}s to retrieve {len(result['value'])} items")
+
+    entity_definitions = result['value']
+    for entity in entity_definitions:
+        entity_logical_name = entity['LogicalName']
+        entity_attributes = entity['Attributes']
+        for attribute in entity_attributes:
+            print(attribute)
+            #($select=ColumnNumber;LogicalName;AttributeType;MaxLength)
+
+    with open('entity_definitions.json', 'w', encoding='utf-8') as f:
+        json.dump(entity_definitions, f, ensure_ascii=False, indent=4)
+
+    print("==> Done!")
+
+
+if __name__ == '__main__':
+    main()
